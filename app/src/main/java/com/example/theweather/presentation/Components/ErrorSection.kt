c63d4d1fec2b3e20a5f2d72a1d312dc768fd1396
Rafael Lemos <rhplemos@gmail.com>
2024-04-21 16:03:02 -0300

    feat: state components

---
+++ app/src/main/java/com/example/theweather/presentation/Components/ErrorSection.kt
@@ -0,0 +1,19 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun ErrorSection(errorMessage: String) {
+    return Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = errorMessage, color = Color.White)
+    }
+}
---
+++ app/src/main/java/com/example/theweather/presentation/Components/WeatherSection.kt
@@ -0,0 +1,19 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.theweather.data.models.WeatherResult
+
+@Composable
+fun WeatherSection(weatherResponse: WeatherResult) {
+    return Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = weatherResponse.toString())
+    }
+}
