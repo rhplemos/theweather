bef555b705f4ae2f2234d29c6231560f00f0d773
Rafael Lemos <rhplemos@gmail.com>
2024-04-21 10:45:04 -0300

    fix: build fix

---
+++ app/src/main/java/com/example/theweather/presentation/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.theweather.presentation
+
+import android.Manifest
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.plcoding.weatherapp.presentation.ui.theme.DarkBlue
+import com.plcoding.weatherapp.presentation.ui.theme.DeepBlue
+import com.plcoding.weatherapp.presentation.ui.theme.WeatherAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            CircularProgressIndicator(
+
+            )
+
+        }
+    }
+}
