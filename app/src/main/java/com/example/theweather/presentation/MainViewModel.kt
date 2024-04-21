aa51a76a1c607ec0ab2121f56748c72d074d88b0
Rafael Lemos <rhplemos@gmail.com>
2024-04-21 12:34:00 -0300

    feat my viewmodel

---
+++ app/src/main/java/com/example/theweather/presentation/MainViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.theweather.presentation
+
+import androidx.lifecycle.ViewModel
+
+enum class STATE {
+    LOADING,
+    SUCCESS,
+    FAILED
+}
+
+class MainViewModel: ViewModel() {
+}
