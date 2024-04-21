f2ee04733c942ea8a71a78acb55037e44d6af21b
Rafael Lemos <rhplemos@gmail.com>
2024-04-21 17:03:33 -0300

    feat: extensions

---
+++ app/src/main/java/com/example/theweather/domain/util/Utils.kt
@@ -0,0 +1,20 @@
+package com.example.theweather.domain.util
+
+import java.text.SimpleDateFormat
+
+class Utils {
+    companion object {
+        fun timestampToHumanDate(timeStamp: Long, format: String): String {
+            val sdf = SimpleDateFormat(format)
+            return sdf.format(timeStamp * 1000)
+        }
+
+        fun buildIcon(icon: String, isBigSize: Boolean = true): String {
+            return if (isBigSize) {
+                "https://openweathermap.org/img/wn/$icon@4x.png"
+            } else {
+                "https://openweathermap.org/img/wn/$icon.png"
+            }
+        }
+    }
+}
