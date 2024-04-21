139a9161654b5b1be26f232a5ba9b74255bf48c0
Rafael Lemos <rhplemos@gmail.com>
2024-04-21 14:25:23 -0300

    feat: viewmodel search methods

--- app/src/main/java/com/example/theweather/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/theweather/presentation/MainViewModel.kt
@@ -1,6 +1,16 @@
 package com.example.theweather.presentation
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.theweather.data.models.ForecastResult
+import com.example.theweather.data.models.LatLng
+import com.example.theweather.data.models.WeatherResult
+import com.example.theweather.data.remote.RetrofitClient
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 enum class STATE {
     LOADING,
@@ -9,4 +19,38 @@ enum class STATE {
 }
 
 class MainViewModel: ViewModel() {
+    var state by mutableStateOf(STATE.LOADING)
+    var weatherResponse: WeatherResult by mutableStateOf(WeatherResult())
+    var forecastResponse: ForecastResult by mutableStateOf(ForecastResult())
+    var errorMessage: String by mutableStateOf("")
+
+    fun getWeatherByLocation(latLng: LatLng){
+        viewModelScope.launch {
+            state = STATE.LOADING
+            val apiService = RetrofitClient.getInstance()
+            try {
+                val apiResponse = apiService.getWeatherData(latLng.lat, latLng.lng)
+                weatherResponse = apiResponse
+                state = STATE.SUCCESS
+            } catch (e: Exception) {
+                errorMessage = e.message.toString()
+                state = STATE.FAILED
+            }
+        }
+    }
+
+    fun getForecastByLocation(latLng: LatLng) {
+        viewModelScope.launch {
+            state = STATE.LOADING
+            val apiService = RetrofitClient.getInstance()
+            try {
+                val apiResponse = apiService.getForecast(latLng.lat, latLng.lng)
+                forecastResponse = apiResponse
+                state = STATE.SUCCESS
+            } catch (e: Exception) {
+                errorMessage = e.message.toString()
+                state = STATE.FAILED
+            }
+        }
+    }
 }
