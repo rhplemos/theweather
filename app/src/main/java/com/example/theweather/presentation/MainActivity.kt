998819b14c18421453fa532064c294541907f33f
Rafael Lemos <rhplemos@gmail.com>
2024-04-21 11:09:44 -0300

    refactor: kotlin version to compile compose

--- app/src/main/java/com/example/theweather/presentation/MainActivity.kt
+++ app/src/main/java/com/example/theweather/presentation/MainActivity.kt
@@ -1,24 +1,17 @@
 package com.example.theweather.presentation
 
-import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.viewModels
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import com.plcoding.weatherapp.presentation.ui.theme.DarkBlue
-import com.plcoding.weatherapp.presentation.ui.theme.DeepBlue
-import com.plcoding.weatherapp.presentation.ui.theme.WeatherAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -27,11 +20,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Text(
-                text = "text",
-                color = Color.Red,
-                textAlign = TextAlign.Center,
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(DarkBlue)
+            ) {
+                Text(
+                    text = "text",
+                    color = Color.Red,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.align(Alignment.End)
+                )
+            }
         }
     }
 }
